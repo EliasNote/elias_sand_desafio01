669cf3ecd0dcd1b7c1e0dce8229dc6c382504064
Elias Mathias Sand <104523320+EliasNote@users.noreply.github.com>
2024-04-09 17:43:18 -0300

    Classe resultado corrigida, atributo resultados do tipo List removido

--- src/main/java/br/com/desafio03/tipos/Resultado.java
+++ src/main/java/br/com/desafio03/tipos/Resultado.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Resultado {
-    private List<Integer> resultados = new ArrayList<>();
     private Integer resultado;
 
     public Resultado() {}
